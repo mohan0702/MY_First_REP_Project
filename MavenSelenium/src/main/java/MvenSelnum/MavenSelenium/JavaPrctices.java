f53f831ee0e3b7e41c989acbd80639fdf12c452a
lenovo <lenovo@lenovo-PC>
2020-02-01 11:21:35 +0530

    My first project

---
+++ MavenSelenium/src/main/java/MvenSelnum/MavenSelenium/JavaPrctices.java
@@ -0,0 +1,16 @@
+package MvenSelnum.MavenSelenium;
+
+public class JavaPrctices {
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 5; j++) {
+
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+}
